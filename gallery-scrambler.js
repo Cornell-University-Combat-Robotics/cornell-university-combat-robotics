6b888262e61a6437532b4fe7be22eef239ac6be9
lee48 <lee48@cornell.edu>
2022-02-25 17:00:20 -0500

    Added more gallery pics, accessibility page

--- gallery-scrambler.js
+++ gallery-scrambler.js
@@ -8,14 +8,24 @@ const imagePaths = [
   'prototype',
   'quad',
   'receipt',
-  'seesaw'
+  'seesaw',
+  'bois',
+  'docs',
+  'idknames',
+  'laptops',
+  'rightouterwall',
+  'screws',
+  'snip',
+  'wall2',
+  'kinetic-rolling2',
+  'minimanny'
 ];
 imagePaths.sort(() => Math.random() < 0.5)
 function rotateAndSetImages() {
   var pics = document.getElementsByClassName("gallery-img")
   console.log(pics)
   Array.from(pics).forEach((pic) => {
-    var randomNum = Math.random() * 20 - 10
+    var randomNum = Math.random() * 12 - 6
     pic.style.setProperty("transform", `rotate(${randomNum}deg)`)
     var imagePath = imagePaths.pop()
     pic.setAttribute('src', './gallery-pics/' + imagePath + '.jpg')
