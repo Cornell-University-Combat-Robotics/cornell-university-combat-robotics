60e049dffa61551a3029f54385a187b8aef73045
lee48 <lee48@cornell.edu>
2022-03-14 12:48:43 -0400

    Added bios to team leads

--- scripts/team-bios.js
+++ scripts/team-bios.js
@@ -1,6 +1,11 @@
 const bios = {
     "name" : "bio",
-    "sofie" : "Outside of CRC, I am involved in the Engineering Leadership Program. In 2020, I took the leadership courses, and since finishing the courses, I have been trained in leadership coaching and will be teaching some teamwork and leadership seminars in 2022. I also play the flute in the Big Red Marching Band!"
+    "sofie" : "Outside of CRC, I am involved in the Engineering Leadership Program. In 2020, I took the leadership courses, and since finishing the courses, I have been trained in leadership coaching and will be teaching some teamwork and leadership seminars in 2022. I also play the flute in the Big Red Marching Band!",
+    "bruno" : "I love running  distances, playing video games, reading manga, and watching anime!",
+    "ricky" : "I am also a member of Cornell Shogi Club.",
+    "michelle" : "Outside of CRC, I'm involved in BreakFree, a dance team, and Alpha Chi Omega, a sorority. ",
+    "rochelle" : "I am a member of Cornell Chamber Flute Ensemble and College Mentors for Kids.",
+    "edith" : "I work in the Lammerding Lab, DEBUT (Design by Biomedical Undergraduate Teams), Alpha Phi Omega, Cornell Writing Center! I enjoy hosting movie nights!",
 }
 
 let flip = (card) => {
