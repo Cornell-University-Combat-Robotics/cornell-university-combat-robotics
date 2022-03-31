bf56566e57fcd5800b26b9a2d33eaf0ab56af8c2
lucieneckert <lucien.eckert@gmail.com>
2022-03-31 18:52:18 -0400

    Added more gallery pictures

--- scripts/gallery-scrambler.js
+++ scripts/gallery-scrambler.js
@@ -1,28 +1,41 @@
 let imagePaths = []
+let imagePathsToAlt = {
+  'box': "team members assembling a box",
+  'firmware-0': "the firmware team working",
+  'firmware-1': "the firmware team working", 
+  'firmware-2': "the firmware team working",
+  'firmware-3': "the firmware team working",
+  'manny': "one team member dressing up as manny",
+  'prototype': "tea member holding up a prototype",
+  'quad': "team members hanging out on the quad",
+  'receipt': "a long receipt from a team dinner",
+  'seesaw': "team members playing on a seesaw",
+  'bois': "team members vibing in the shop",
+  'docs': "documentation and lance",
+  'idknames': "a happy team member",
+  'laptops': "a laptop-heavy work session",
+  'rightouterwall': "the right outer wall of lance",
+  'screws': "team members organizing screws",
+  'snip': "team member cutting foam",
+  'wall2': "assembly of one of lance's walls",
+  'kinetic-rolling2': "kinetic team members",
+  'minimanny' : "A small 3D printed manny",
+  'inventory': "team members at an 'inventory party'",
+  'weapondemo': "first demonstration of lance's weapon",
+  'unamused': "team member working on lance",
+  "promo": "sportsman team posing with our promo cards",
+  "arduino": "firmware debugging an arduino",
+  "richard": "kinetic posing with richard",
+  "bizsolder": "business and media soldering",
+  "itsrichard": "team members posing with richard",
+  "hat": "spencer gets a new hat",
+  "testbox": "assembly of our testbox",
+  "stik": "the death of big stick during testing",
+  "cardpattern": "our promotional testing event cards"
+};
 const refreshImagePaths = () => {
-  imagePaths = [
-    'box',
-    'firmware-0',
-    'firmware-1',
-    'firmware-2',
-    'firmware-3',
-    'manny',
-    'prototype',
-    'quad',
-    'receipt',
-    'seesaw',
-    'bois',
-    'docs',
-    'idknames',
-    'laptops',
-    'rightouterwall',
-    'screws',
-    'snip',
-    'wall2',
-    'kinetic-rolling2',
-    'minimanny'
-  ];
-  imagePaths.sort(() => Math.random() < 0.5)
+  imagePaths = Object.keys(imagePathsToAlt)
+  imagePaths.sort((a, b) =>  Math.random() - 0.5)
 }
 function rotateAndSetImages() {
   refreshImagePaths();
@@ -30,9 +43,10 @@ function rotateAndSetImages() {
   console.log(pics)
   Array.from(pics).forEach((pic) => {
     var randomNum = Math.random() * 12 - 6
-    pic.style.setProperty("transform", `rotate(${randomNum}deg)`)
+    //pic.style.setProperty("transform", `rotate(${randomNum}deg)`)
     var imagePath = imagePaths.pop()
     pic.setAttribute('src', './gallery-pics/' + imagePath + '.jpg')
+    pic.setAttribute('alt', imagePathsToAlt[imagePath])
     console.log(pic)
   })
 }
